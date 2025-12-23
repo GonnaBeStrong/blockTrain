b208ff4cc8e1147d028194ec2b08aa7e949d0599
yyz <2663167979@qq.com>
2025-12-23 15:59:20 +0800

    1

--- src/main/java/com/sixoneseven/blocktrain/config/BlockTrainApplication.java
+++ src/main/java/com/sixoneseven/blocktrain/config/BlockTrainApplication.java
@@ -1,4 +1,4 @@
-package com.sixoneseven.blocktrain;
+package com.sixoneseven.blocktrain.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
