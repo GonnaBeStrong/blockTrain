20fd231d33a256239a1d2852366f23e22bc5b38d
yyz <2663167979@qq.com>
2025-12-23 15:33:29 +0800

    1

---
+++ src/main/java/com/sixoneseven/blocktrain/BlockTrainApplication.java
@@ -0,0 +1,13 @@
+package com.sixoneseven.blocktrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockTrainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlockTrainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sixoneseven/blocktrain/BlockTrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sixoneseven.blocktrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockTrainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
